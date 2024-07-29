2361dd14325a3b82da62de2773df84b8aaf9b643
John <jbidlack@ccbcmd.edu>
2024-07-29 08:41:58 -0400

    Added a backend. Cleaned up extra files and code no longer needed. NOT Production ready as more cleanup needed.

---
+++ backend/backend.js
@@ -0,0 +1,19 @@
+const env = require('dotenv');
+const { Pool } = require('pg');
+const path = require('path');
+const envPath = path.resolve(__dirname, '../.env');
+env.config({path:envPath});
+
+
+const pool = new Pool({
+    connectionString: process.env.NODE_SB,
+    ssl: {
+        rejectUnauthorized: false
+    }
+    
+});
+
+module.exports = {
+    query: (text, params) => pool.query(text, params),
+};
+
