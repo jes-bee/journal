84cee9b7782432d76845da52da5c1eaa063f3277
John <jbidlack@ccbcmd.edu>
2024-08-16 14:12:51 -0400

    double aaaaaaaaaaaa

--- api/app.js
+++ api/app.js
@@ -2,15 +2,14 @@ const path = require('path');
 const cors = require('cors');
 const env = require('dotenv');
 const express = require('express');
-const router = express.Router();
 const db = require('./backend');
 const crypto = require('crypto');
-const app = express();
+const router = express.Router();
 const envPath = path.resolve(__dirname, '../.env');
 env.config({path:envPath});
 
-app.use(express.json());
-app.use(cors());
+router.use(express.json());
+router.use(cors());
 
 function decrypt(text){
     const algorithm = 'aes-256-ctr';
@@ -30,7 +29,7 @@ function decrypt(text){
     return decrypted.toString();
 }
 
-app.post('/prompt', async (req, res) => {
+router.post('/prompt', async (req, res) => {
     const { category } = req.body;
 
     if (!category){
@@ -60,7 +59,7 @@ app.post('/prompt', async (req, res) => {
     }
 });
 
-app.get('/randPrompts', async(req, res) => {
+router.get('/randPrompts', async(req, res) => {
     try {
         const result = await db.query(
             'select prompt from prompt '+
